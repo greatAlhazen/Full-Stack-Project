910f7fec03dbd1f857370b6ea547122613511439
EnesUraz <axeprime91@gmail.com>
2023-04-12 11:32:56 +0300

    handle stream data

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+let resultsPlanet = [];
+
+// control habitable planet with some suggestion values
+const isHabitable = (planet) => {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+};
+
+// return a readeble stream
+fs
+	.createReadStream('planets_data.csv')
+	.pipe(
+		// connect readeble stream to writable stream
+		parse({
+			// comment block in file
+			comment: '#',
+			// for return key-value pairs
+			columns: true
+		})
+	)
+	.on('data', (data) => {
+		// event when data comes in
+		if (isHabitable(data)) {
+			resultsPlanet.push(data);
+		}
+	})
+	.on('error', (error) => {
+		// event when error occured
+		console.log(error);
+	})
+	.on('end', () => {
+		// event when stream end
+		const finalResult = resultsPlanet.map((item) => {
+			return item['kepler_name'];
+		});
+		console.log(finalResult);
+		console.log(`${resultsPlanet.length} planets found`);
+	});
