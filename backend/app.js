712bf83a69006a97c186061fd39226a0ae6c8863
EnesUraz <axeprime91@gmail.com>
2023-04-19 11:02:30 +0300

    automate app,handle logs

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 // create and handle server framework
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 
 //initialize app
 const app = express();
@@ -17,6 +19,16 @@ app.use(
 
 // middleware that parse json object
 app.use(express.json());
+
+// logging middleware
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// serving website
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // planets route middleware
 app.use('/planets', routerPlanets);
