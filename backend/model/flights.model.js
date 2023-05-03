47f904a0a0265bf5bcf8fdbdb9bdb4dea72e2280
EnesUraz <axeprime91@gmail.com>
2023-05-03 09:27:23 +0300

    get data from api and save to database

--- backend/model/flights.model.js
+++ backend/model/flights.model.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const Flights = require('./flights.mongo');
 const Planets = require('./planets.mongo');
 
@@ -15,6 +16,58 @@ const flight = {
 
 save(flight);
 
+const SPACEX_API_URL = 'https://api.spacexdata.com/v5/launches/query';
+
+// load data from api
+async function loadSpaceXApi() {
+    const response = await axios.post(SPACEX_API_URL, {
+        query: {},
+        options: {
+          pagination: false,
+          populate: [
+            {
+              path: 'rocket',
+              select: {
+                name: 1
+              }
+            },
+            {
+              path: 'payloads',
+              select: {
+                'customers': 1
+              }
+            }
+          ]
+        }
+      });
+
+      if(response.status !== 200){
+        console.log('Load data failed');
+        throw new Error('Load data failed');
+      }
+
+      const flightDocs = response.data.docs;
+      // get all data one by one and save to database
+      for(const flightDoc of flightDocs){
+        const payloads = flightDoc['payloads'];
+        const customers = payloads.flatMap((payload) =>{
+            return payload['customers']
+        })
+        const flight = {
+            launchNumber:flightDoc['flight_number'],
+            mission:flightDoc['name'],
+            rocket:flightDoc['rocket']['name'],
+            date:flightDoc['date_local'],
+            upcoming:flightDoc['upcoming'],
+            success:flightDoc['success'],
+            customers
+        }
+
+        console.log(flight.mission);
+        await save(flight);
+      }
+}
+
 
 async function getAllFlights () {
     const response = await Flights.find({},{'_id':0,'__v':0});
@@ -31,6 +84,14 @@ async function getLatestNumber(){
 
 // addFlight to map object
 async function setFlight(flight){
+    const planet = await Planets.findOne({
+        keplerName:flight.destination
+    });
+
+    if(!planet){
+        throw new Error('Planet not found');
+    }
+
     const launchNumber = await getLatestNumber() + 1;
     const newFlight = Object.assign(flight,{
         upcoming:true,
@@ -50,13 +111,6 @@ async function existFlight(id){
 
 // save flight in mongo
 async function save(flight){
-    const planet = await Planets.findOne({
-        keplerName:flight.destination
-    });
-
-    if(!planet){
-        throw new Error('Planet not found');
-    }
     await Flights.updateOne({
         launchNumber:flight.launchNumber,
     },flight,
@@ -79,5 +133,6 @@ module.exports = {
     getAllFlights,
     setFlight,
     existFlight,
-    abortFlight
+    abortFlight,
+    loadSpaceXApi,
 }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const {mongoConnection} = require('./config/mongoDB.config');
 
 const app = require('./app');
 const { loadData } = require('./model/planets.model');
+const { loadSpaceXApi } = require('./model/flights.model');
 
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
@@ -12,6 +13,7 @@ const server = http.createServer(app);
 // before restart server load valueable data
 async function serverStart() {
 	await mongoConnection();
+	await loadSpaceXApi();
 	
 	await loadData();
 
